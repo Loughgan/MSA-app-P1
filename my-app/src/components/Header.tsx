45e2d9c31be9a23c055ff5a90505ed7ca93ad1b2
Loughgan <loganlogan123@live.com>
2018-09-10 22:09:28 +1200

    Redid and optimized for my application

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,24 +1,17 @@
+import {AppBar, Toolbar, Typography} from '@material-ui/core/';
 import * as React from 'react';
-import { Nav, Navbar, NavItem } from 'react-bootstrap';
-import { IndexLinkContainer } from "react-router-bootstrap";
 import { Link } from 'react-router-dom';
 
 export const Header: React.StatelessComponent<{}> = () => {
     return (
-        <Navbar>
-            <Navbar.Header>
-                <Navbar.Brand>
-                    <Link to="/">dankNotDank</Link>
-                </Navbar.Brand>
-            </Navbar.Header>
-            <Nav>
-                <IndexLinkContainer to="/FirstComponent">
-                    <NavItem>Page 1</NavItem>
-                </IndexLinkContainer>
-                <IndexLinkContainer to="/SecondComponent">
-                    <NavItem>Page 2</NavItem>
-                </IndexLinkContainer>
-            </Nav>
-        </Navbar>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="display2" color="inherit">
+                        <Link style={{color: "white"}} to="/">Logan's Lyrical Library</Link>
+                        <Link style={{color: "white"}} to="/FirstComponent"> | About | </Link>
+                        <Link style={{color: "white"}} to="/SecondComponent"> Page 2 </Link>
+                    </Typography>
+                </Toolbar>
+            </AppBar>
     );
 }
