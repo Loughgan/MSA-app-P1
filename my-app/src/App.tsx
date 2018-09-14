eb42b9793ecda405ca8ae5276580e2cd1b019511
Loughgan <loganlogan123@live.com>
2018-09-15 01:32:43 +1200

    Slowly getting somewhere

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,48 @@
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import * as React from 'react';
+// import { withStyles } from '@material-ui/core/styles';
+// import MenuItem from '@material-ui/core/MenuItem';
 import './App.css';
 
+
+
 export default class App extends React.Component<{}> {
+  
+  public state = {
+    artist: '',
+    lyrics: '',
+    song: '',
+  };
+
+  public changeArtist(value: string){
+    this.setState({artist: value});
+  }
+
   public render() {
     return (
       <div className="container-fluid">
       <div className="centreText">
         {/* React components must have a wrapper node/element */}
-        <h1>( ͡° ͜ʖ ͡°)</h1>
+        <h1>{this.state.artist}</h1>
+        <TextField
+          id="with-placeholder"
+          label="Enter the Artist's Name"
+          value={this.state.artist}
+          // onChange={this.changeArtist(":(")}
+          placeholder="Artist Name"
+          margin="normal"
+        />
+        <TextField
+          id="with-placeholder"
+          label="Enter the Song's Name"
+          value={this.state.song}
+          placeholder="Song Name"
+          margin="normal"
+        />
+        <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
       </div>
     </div>
     );
