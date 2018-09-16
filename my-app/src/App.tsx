8ef655e944f90c7874db3eb31faf791ea5ea40bc
Loughgan <loganlogan123@live.com>
2018-09-16 15:06:33 +1200

    error handling added

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -24,23 +24,40 @@ export default class App extends React.Component<{}, IState> {
     }
   }
 
-  // public changeArtist(value: string){
-  //  this.setState({artist: value});
-  // }
-
-  public getRate = async (e: any) => {
+  public getLyrics = async (e: any) => {
     e.preventDefault();
-    const artist = e.target.elements.newArtist.value;
-    const song = e.target.elements.newSong.value;
+    const theArtist = e.target.elements.newArtist.value;
+    const theSong = e.target.elements.newSong.value;
     // tslint:disable-next-line:no-console
-    console.log(artist);
-    const API = await fetch(`https://orion.apiseeds.com/api/music/lyric/${artist}/${song}?apikey=snMiaZzHvKn3lVDz6SArWbItrgTiBdhBluLZ5o3ay4QOZdP2wWQsjuaDLUIzlRkB`);
-    const data = await API.json();
-    // tslint:disable-next-line:no-console
-    console.log(data.result.track.text);
-    this.setState({
-      lyrics: data.result.track.text// .replace(/\n/gi, "<br/>")
-    });
+    console.log(theArtist);
+    if(theArtist && theSong){
+      const API = await fetch(`https://orion.apiseeds.com/api/music/lyric/${theArtist}/${theSong}?apikey=snMiaZzHvKn3lVDz6SArWbItrgTiBdhBluLZ5o3ay4QOZdP2wWQsjuaDLUIzlRkB`);
+      const data = await API.json();
+      // tslint:disable-next-line:no-console
+      // console.log(data.result.track.text);
+
+      if(data.result){// (data.result.artist.name && data.result.track.text && data.result.track.name){
+        this.setState({
+          artist: data.result.artist.name,
+          lyrics: data.result.track.text,// .replace(/\n/gi, "<br/>"
+          song: data.result.track.name
+        });
+      }
+      else {
+        this.setState({
+          artist: undefined,
+          lyrics: "ARTIST AND/OR SONG NOT RECOGNIZED. PLEASE CHECK SPELLING AND TRY AGAIN.\nIF YOU ARE CERTAIN IT IS CORRECT, LOGAN MAY NOT HAVE THE LYRICS IN HIS LIBRARY.",
+          song: undefined
+        })
+      }
+    }
+    else {
+      this.setState({
+        artist: undefined,
+        lyrics: "PLEASE ENTER AN ARTIST AND A NAME OF A SONG",
+        song: undefined
+      })
+    }
   }
 
   public render() {
@@ -51,23 +68,33 @@ export default class App extends React.Component<{}, IState> {
         <div className="container-fluid">
         <div className="centreText">
           {/* React components must have a wrapper node/element */}
-          <form onSubmit={this.getRate}>
+          <div style={{margin: '10px'}}>
+            Enter the name of an artist and a song by them to get the lyrics!
+          </div>
+          <form onSubmit={this.getLyrics}>
             <TextField
               label="Enter the Artist's Name"
               name = "newArtist"
               placeholder="Artist Name"
               margin="normal"
+              style={{margin: '15px'}}
             />
             <TextField
               label="Enter the Song's Name"
               name = "newSong"
               placeholder="Song Name"
               margin="normal"
+              style={{margin: '15px'}}
             />
+            <div>
             <Button variant="contained" type="submit" color="secondary">
             Submit
             </Button>
+            </div>
           </form>
+          <h1>
+            {this.state.artist} - {this.state.song}
+          </h1>
           <div className="display-linebreak">
           <p>{this.state.lyrics}</p>
           </div>
--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,16 +1,17 @@
 import {AppBar, Toolbar, Typography} from '@material-ui/core/';
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 export const Header: React.StatelessComponent<{}> = () => {
     return (
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="display2" color="inherit">
-                        <Link style={{color: "white"}} to="/">Logan's Lyrical Library</Link>
+                        Logan's Lyrical Library
                     </Typography>
                 </Toolbar>
             </AppBar>
+            // <Link style={{color: "white"}} to="/">Logan's Lyrical Library</Link>
             //            <Link style={{color: "white"}} to="/FirstComponent"> | About | </Link>
             //            <Link style={{color: "white"}} to="/SecondComponent"> All My Ex's Live In Texas </Link>            
     );
